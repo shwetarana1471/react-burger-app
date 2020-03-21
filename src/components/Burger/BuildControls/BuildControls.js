bb05e2b4c52c898e5ce997ef11ab44337268ca44
Shweta Rana <Srana@BHAVNACORP.NET>
2020-03-21 10:27:29 +0530

    sevent commit up to date

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -9,12 +9,19 @@ const controls = [
     {label:'Meat' , type:'meat'},
 ];
 
-const buildControls = (props) => {
+const buildControls = (props) => (
     <div className={classes.BuildControls}> 
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
         {controls.map(ctrl => (
-            <BuildControl key={ctrl.label} label={ctrl.label}/>
+            <BuildControl 
+                key={ctrl.label} 
+                label={ctrl.label}
+                type={ctrl.type}
+                added={() => props.ingredientAdded(ctrl.type)} 
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]} />
         ))}
     </div>
-};
+);
 
 export default buildControls;
