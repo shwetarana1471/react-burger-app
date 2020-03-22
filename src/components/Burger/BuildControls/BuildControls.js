49a46fdf86529098f91afca0584934d9289dcef7
Shweta Rana <Srana@BHAVNACORP.NET>
2020-03-22 21:03:44 +0530

    eight

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -21,6 +21,7 @@ const buildControls = (props) => (
                 removed={() => props.ingredientRemoved(ctrl.type)}
                 disabled={props.disabled[ctrl.type]} />
         ))}
+        <button>ORDER NOW</button>
     </div>
 );
 
